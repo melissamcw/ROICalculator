e6e2db777ecf40377b227a50586d005457f44343
Ryan Brideau <ryan.brideau@gmail.com>
2013-08-14 17:15:23 -0300

    Used objects instead of plain arrays to make everybody's life easier

--- js/roifunctions.js
+++ js/roifunctions.js
@@ -1,35 +1,103 @@
-// This is an ROI calculator written for CRSTL Solutions
 // All of the calculations can be found at the bottom of the document.
 // The variables are collected at the top for easy editing and changing.
 // The naming system used is quite straight forward, and is meant to 
 // give consistency between the slider IDs, the labels that correspond to them
-// and the variables that represent them in code. This was written in jQuery as
-// of March 5th, 2012, and takes advantage of the jQueryUI for the sliders.
-// The dropdowns are just regular dropdowns, but are bound to jQuery events
-// so that they can update the totals live.
+// and the variables that represent them in code.
 
 // A namespace for storing all the variables used in the calculations
 $.roi = {
 	// arrExample : [ item ID, item label ID, value ]
 	// These variables are in the same order as they are on the website to make it easier to read them
-	arrLawStaff : ["#lawstaff", "#lblLawStaff", 2],
-	arrLawCost: ["#lawcost", "#lblLawCost", 150000],
-	arrSuppStaff: ["#suppstaff", "#lblSuppStaff", 2],
-	arrSuppCost: ["#suppcost", "#lblSuppCost", 60000],
-	arrMonitoring : ["#monitoring", "#lblMonitoring", 0],
-	arrUpdating : ["#updating", "#lblUpdating", 0],
-	arrCounselAdvice : ["#counseladvice", "#lblCounselAdvice", 0],
-	arrFlagGaps : ["#flaggaps", "#lblFlagGaps", 0],
-	arrReviewing : ["#reviewing", "#lblReviewing", 0],
-	arrPreparing : ["#preparing", "#lblPreparing", 0],
-	arrCollecting : ["#collecting", "#lblCollecting", 0],
-	arrFlagGapsCO : ["#flaggapsCO", "#lblFlagGapsCO", 0],
-	arrDocumenting : ["#documenting", "#lblDocumenting", 0],
-	arrFollowup : ["#followup", "#lblFollowUp", 0],
-	arrTotalHours : ["#totalhours", "#lblTotalHours", 0],
-	arrTotalCost: ["#totalcost", "#lblTotalCost", 0],
-	arrTimeSaved: ["#timesaved", "#lblTimeSaved", 0],
-	arrCostSaved: ["#costsaved", "#lblCostSaved", 0]
+	arrLawStaff : {
+		slider: "#lawstaff",
+		label: "#lblLawStaff",
+		value: 2
+	},
+	arrLawCost: {
+		slider: "#lawcost", 
+		label: "#lblLawCost", 
+		value: 150000
+	},
+	arrSuppStaff: {
+		slider: "#suppstaff", 
+		label: "#lblSuppStaff", 
+		value: 2
+	},
+	arrSuppCost: {
+		slider: "#suppcost", 
+		label: "#lblSuppCost", 
+		value: 60000
+	},
+	arrMonitoring : {
+		slider: "#monitoring", 
+		label: "#lblMonitoring", 
+		value: 0
+	},
+	arrUpdating : {
+		slider: "#updating", 
+		label: "#lblUpdating", 
+		value: 0
+	},
+	arrCounselAdvice : {
+		slider: "#counseladvice", 
+		label: "#lblCounselAdvice", 
+		value: 0
+	},
+	arrFlagGaps : {
+		slider: "#flaggaps", 
+		label: "#lblFlagGaps", 
+		value: 0
+	},
+	arrReviewing : {
+		slider: "#reviewing", 
+		label: "#lblReviewing", 
+		value: 0
+	},
+	arrPreparing : {
+		slider: "#preparing", 
+		label: "#lblPreparing", 
+		value: 0
+	},
+	arrCollecting : {
+		slider: "#collecting", 
+		label: "#lblCollecting", 
+		value: 0
+	},
+	arrFlagGapsCO : {
+		slider: "#flaggapsCO", 
+		label: "#lblFlagGapsCO", 
+		value: 0
+	},
+	arrDocumenting : {
+		slider: "#documenting", 
+		label: "#lblDocumenting", 
+		value: 0
+	},
+	arrFollowup : {
+		slider: "#followup", 
+		label: "#lblFollowUp", 
+		value: 0
+	},
+	arrTotalHours : {
+		slider: "#totalhours", 
+		label: "#lblTotalHours", 
+		value: 0
+	},
+	arrTotalCost: {
+		slider: "#totalcost", 
+		label: "#lblTotalCost", 
+		value: 0
+	},
+	arrTimeSaved: {
+		slider: "#timesaved", 
+		label: "#lblTimeSaved", 
+		value: 0
+	},
+	arrCostSaved: {
+		slider: "#costsaved", 
+		label: "#lblCostSaved", 
+		value: 0
+	}
 };
 			
 $(function(){
@@ -46,22 +114,22 @@ $(function(){
 	
 	// The first line ensures that the code executes when their value is changed.
 	$("#lawstaff").change(function () {
-	$.roi.arrLawStaff[2] = $(this).val(); //Updates the value element of the appropriate array above.
+	$.roi.arrLawStaff.value = $(this).val(); //Updates the value element of the appropriate array above.
 		calcOutput(); //Calls the function to update the totals
 	});
 	
 	$("#lawcost").change(function () {
-	$.roi.arrLawCost[2] = $(this).val();
+	$.roi.arrLawCost.value = $(this).val();
 		calcOutput();
 	});
 	
 	$("#suppstaff").change(function () {
-	$.roi.arrSuppStaff[2] = $(this).val();
+	$.roi.arrSuppStaff.value = $(this).val();
 		calcOutput();
 	});
 	
 	$("#suppcost").change(function () {
-	$.roi.arrSuppCost[2] = $(this).val();
+	$.roi.arrSuppCost.value = $(this).val();
 		calcOutput();
 	});
 				
@@ -164,13 +232,13 @@ function SlideFunction(arrSlider, ui) {
 	if (ui.value == -1) {
 		ui.value = 0;
 		};
-	$(arrSlider[1]).val(ui.value + " Hours" );
+	$(arrSlider.label).val(ui.value + " Hours" );
 };
 
 // Activated when the slider stops. Updates the value of the slider, and calls the function that calculates the output.
 // Do not combine this with the SlideFunction function, or else the values will always be one step behind.
 function SlideChange(arrSlider, ui) {
-	arrSlider[2] = $(arrSlider[0]).slider("option", "value");
+	arrSlider.value = $(arrSlider.slider).slider("option", "value");
 	calcOutput();
 };
 
@@ -180,27 +248,27 @@ function SlideChange(arrSlider, ui) {
 		var iTotalLawyerHours, iTotalSuppHours, iTotalLawyerCost, iTotalSuppCost;
 				
 		// Lawyer Calculations
-		// The [2] refers to the value for a certain item, so naturally it is found in each of the variables that are in an array.
+		// The .value refers to the value for a certain item, so naturally it is found in each of the variables that are in an array.
 		// Go to the top of this file to see the various arrays and elements. 2000 is the approximate number of working hours
 		// in a year. 50 is approximately 52, the number of weeks in a year, and gives round numbers.
-		iTotalLawyerHours = $.roi.arrLawStaff[2] * ( $.roi.arrMonitoring[2] + $.roi.arrUpdating[2] + $.roi.arrCounselAdvice[2] + $.roi.arrFlagGaps[2] + $.roi.arrReviewing[2] ) * 50;
-		iTotalLawyerCost = iTotalLawyerHours * $.roi.arrLawCost[2] / 2000;
+		iTotalLawyerHours = $.roi.arrLawStaff.value * ( $.roi.arrMonitoring.value + $.roi.arrUpdating.value + $.roi.arrCounselAdvice.value + $.roi.arrFlagGaps.value + $.roi.arrReviewing.value ) * 50;
+		iTotalLawyerCost = iTotalLawyerHours * $.roi.arrLawCost.value / 2000;
 		
 		// Support Calculations
-		iTotalSuppHours = $.roi.arrSuppStaff[2] * ( $.roi.arrPreparing[2] + $.roi.arrCollecting[2] + $.roi.arrFlagGapsCO[2] + $.roi.arrDocumenting[2] + $.roi.arrFollowup[2] ) * 50;
-		iTotalSuppCost = iTotalSuppHours * $.roi.arrSuppCost[2] / 2000;
+		iTotalSuppHours = $.roi.arrSuppStaff.value * ( $.roi.arrPreparing.value + $.roi.arrCollecting.value + $.roi.arrFlagGapsCO.value + $.roi.arrDocumenting.value + $.roi.arrFollowup.value ) * 50;
+		iTotalSuppCost = iTotalSuppHours * $.roi.arrSuppCost.value / 2000;
 		
 		// End totals
-		$.roi.arrTotalHours[2] = iTotalLawyerHours + iTotalSuppHours;
-		$.roi.arrTotalCost[2] = iTotalLawyerCost + iTotalSuppCost;
-		$.roi.arrTimeSaved[2] = Math.ceil(0.4*(iTotalLawyerHours + iTotalSuppHours));
-		$.roi.arrCostSaved[2] = 0.4*(iTotalLawyerCost + iTotalSuppCost);
+		$.roi.arrTotalHours.value = iTotalLawyerHours + iTotalSuppHours;
+		$.roi.arrTotalCost.value = iTotalLawyerCost + iTotalSuppCost;
+		$.roi.arrTimeSaved.value = Math.ceil(0.4*(iTotalLawyerHours + iTotalSuppHours));
+		$.roi.arrCostSaved.value = 0.4*(iTotalLawyerCost + iTotalSuppCost);
 		
 		// Used to display the totals
-		$($.roi.arrTotalHours[0]).val($.roi.arrTotalHours[2] + " Hours" );
-		$($.roi.arrTotalCost[0]).val("$" + $.roi.arrTotalCost[2]);
-		$($.roi.arrTimeSaved[0]).val($.roi.arrTimeSaved[2] + " Hours" );
-		$($.roi.arrCostSaved[0]).val("$" + $.roi.arrCostSaved[2]);
+		$($.roi.arrTotalHours.slider).val($.roi.arrTotalHours.value + " Hours" );
+		$($.roi.arrTotalCost.slider).val("$" + $.roi.arrTotalCost.value);
+		$($.roi.arrTimeSaved.slider).val($.roi.arrTimeSaved.value + " Hours" );
+		$($.roi.arrCostSaved.slider).val("$" + $.roi.arrCostSaved.value);
 	};
 	
 	
