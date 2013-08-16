f56371d8730587828aecc3daed0ed03d987daf86
Ryan Brideau <ryan.brideau@gmail.com>
2013-08-16 14:06:06 -0300

    Refactored the slider code

--- js/roifunctions.js
+++ js/roifunctions.js
@@ -9,7 +9,7 @@ $.industVar = {
 	// arrExample : [ item ID, item label ID, value ]
 	// These variables are in the same order as they are on the website to make it easier to read them
 	wallet : {
-		low: {
+		low : {
 			airlines: 56,
 			banks: 70,
 			csp: 68,
@@ -23,7 +23,7 @@ $.industVar = {
 			retailers: 60,
 			wcs: 66
 		},
-		high: {
+		high : {
 			airlines: 70,
 			banks: 82,
 			csp: 77,
@@ -36,11 +36,17 @@ $.industVar = {
 			isps: 77,
 			retailers: 69,
 			wcs: 73
-		},
+		}
 	}
 };
 
-$.guiVars = {
+$.dropdownVars = {
+	industry  : {
+		value: "airlines"
+	}
+};
+
+$.sliderVars = {
 	customers  : {
 		slider: "#CustomersSlider",
 		label: "#lblCustomers",
@@ -70,6 +76,19 @@ $.guiVars = {
 		slider: "#CallDurationSlider",
 		label: "#lblCallDuration",
 		value: 300
+	},
+	walletclose : {
+		slider : "#WalletGapSlider",
+		label : "#lblWalletGap",
+		value : 50
+	}
+};
+
+$.fieldVars = {
+	wallet : {
+		laggards : "#WalletLaggards",
+		leaders : "#WalletLeaders",
+		diff : "#WalletDiff"
 	}
 };
 			
@@ -118,140 +137,64 @@ $(function(){
 		step: 10
 	});
 
-	// Dropdown menus
+	$('#WalletGapSlider').slider ({
+		value: 50,
+		min: 0,
+		max: 100,
+		step: 5
+	});
 	
 	// The first line ensures that the code executes when their value is changed.
-	$("#lawstaff").change(function () {
-	$.roi.arrLawStaff.value = $(this).val(); //Updates the value element of the appropriate array above.
+	$("#IndustryDropdown").change(function () {
+		$.dropdownVars.industry.value = $(this).val(); //Updates the value element of the appropriate array above.
 		calcOutput(); //Calls the function to update the totals
 	});
 	
-	$("#lawcost").change(function () {
-	$.roi.arrLawCost.value = $(this).val();
-		calcOutput();
-	});
-	
-	$("#suppstaff").change(function () {
-	$.roi.arrSuppStaff.value = $(this).val();
-		calcOutput();
-	});
-	
-	$("#suppcost").change(function () {
-	$.roi.arrSuppCost.value = $(this).val();
-		calcOutput();
-	});
 				
-	// Sliders
-	$('#CustomersSlider').slider({
-		// Occurs when the user begins to move the slider
-		slide: function( event, ui ) {
-			SlideFunction($.guiVars.customers, ui);
-		},
-		// Occurs when the slider stops moving
-		change: function( event, ui) {
-			SlideChange($.guiVars.customers, ui);
-		}
+	// Tells what to do every time a slider moves or changes.
+	$.each($.sliderVars, function(key, value) {
+		$(value.slider).slider({
+			// Occurs when the user begins to move the slider
+			slide: function( event, ui ) {
+			SlideFunction(value, ui);
+			},
+			// Occurs when the slider stops moving
+			change: function( event, ui) {
+			SlideChange(value, ui);
+			}
+		});
 	});
 
-	$('#RevBasicSlider').slider({
-		// Occurs when the user begins to move the slider
-		slide: function( event, ui ) {
-			SlideFunction($.guiVars.revbasic, ui);
-		},
-		// Occurs when the slider stops moving
-		change: function( event, ui) {
-			SlideChange($.guiVars.revbasic, ui);
-		}
-	});
-
-	$('#RevEnhanSlider').slider({
-		// Occurs when the user begins to move the slider
-		slide: function( event, ui ) {
-			SlideFunction($.guiVars.revenhan, ui);
-		},
-		// Occurs when the slider stops moving
-		change: function( event, ui) {
-			SlideChange($.guiVars.revenhan, ui);
-		}
-	});
-
-	$('#CostPerCallSlider').slider({
-		// Occurs when the user begins to move the slider
-		slide: function( event, ui ) {
-			SlideFunction($.guiVars.costpercall, ui);
-		},
-		// Occurs when the slider stops moving
-		change: function( event, ui) {
-			SlideChange($.guiVars.costpercall, ui);
-		}
-	});
-
-	$('#CallVolumeSlider').slider({
-		// Occurs when the user begins to move the slider
-		slide: function( event, ui ) {
-			SlideFunction($.guiVars.callvolume, ui);
-		},
-		// Occurs when the slider stops moving
-		change: function( event, ui) {
-			SlideChange($.guiVars.callvolume, ui);
-		}
-	});
-
-	$('#CallDurationSlider').slider({
-		// Occurs when the user begins to move the slider
-		slide: function( event, ui ) {
-			SlideFunction($.guiVars.callduration, ui);
-		},
-		// Occurs when the slider stops moving
-		change: function( event, ui) {
-			SlideChange($.guiVars.callduration, ui);
-		}
-	});
-
-// Activated when the slider moves. Updates the label for the slider.
-function SlideFunction(guiVars, ui) {
+	// Activated when the slider moves. Updates the label for the slider.
+	function SlideFunction(guiItem, ui) {
 	// There is a bug in some old browers that make this go negative. This fixes that.
-	if (ui.value == -1) {
-		ui.value = 0;
-		};
-	$(guiVars.label).val(ui.value);
-};
+		if (ui.value == -1) {
+			ui.value = 0;
+			};
+		$(guiItem.label).val(ui.value);
+	};
 
-// Activated when the slider stops. Updates the value of the slider, and calls the function that calculates the output.
-// Do not combine this with the SlideFunction function, or else the values will always be one step behind.
-function SlideChange(arrSlider, ui) {
-	arrSlider.value = $(arrSlider.slider).slider("option", "value");
-	calcOutput();
-};
+	// Activated when the slider stops. Updates the value of the slider, and calls the function that calculates the output.
+	// Do not combine this with the SlideFunction function, or else the values will always be one step behind.
+	function SlideChange(guiItem, ui) {
+		guiItem.value = $(guiItem.slider).slider("option", "value");
+		calcOutput();
+	};
 
-// The formula used to calculate the output value when the slider is moved or a dropdown in changed.
+	// The formula used to calculate the output value when the slider is moved or a dropdown in changed.
 	function calcOutput() {
 	// Local variables to make it easier to break up the calculations. 
-		var iTotalLawyerHours, iTotalSuppHours, iTotalLawyerCost, iTotalSuppCost;
-				
-		// Lawyer Calculations
-		// The .value refers to the value for a certain item, so naturally it is found in each of the variables that are in an array.
-		// Go to the top of this file to see the various arrays and elements. 2000 is the approximate number of working hours
-		// in a year. 50 is approximately 52, the number of weeks in a year, and gives round numbers.
-		iTotalLawyerHours = $.roi.arrLawStaff.value * ( $.roi.arrMonitoring.value + $.roi.arrUpdating.value + $.roi.arrCounselAdvice.value + $.roi.arrFlagGaps.value + $.roi.arrReviewing.value ) * 50;
-		iTotalLawyerCost = iTotalLawyerHours * $.roi.arrLawCost.value / 2000;
+		var iLaggardWallet, iLeaderWallet, iDiffWallet
 		
-		// Support Calculations
-		iTotalSuppHours = $.roi.arrSuppStaff.value * ( $.roi.arrPreparing.value + $.roi.arrCollecting.value + $.roi.arrFlagGapsCO.value + $.roi.arrDocumenting.value + $.roi.arrFollowup.value ) * 50;
-		iTotalSuppCost = iTotalSuppHours * $.roi.arrSuppCost.value / 2000;
-		
-		// End totals
-		$.roi.arrTotalHours.value = iTotalLawyerHours + iTotalSuppHours;
-		$.roi.arrTotalCost.value = iTotalLawyerCost + iTotalSuppCost;
-		$.roi.arrTimeSaved.value = Math.ceil(0.4*(iTotalLawyerHours + iTotalSuppHours));
-		$.roi.arrCostSaved.value = 0.4*(iTotalLawyerCost + iTotalSuppCost);
-		
-		// Used to display the totals
-		$($.roi.arrTotalHours.slider).val($.roi.arrTotalHours.value + " Hours" );
-		$($.roi.arrTotalCost.slider).val("$" + $.roi.arrTotalCost.value);
-		$($.roi.arrTimeSaved.slider).val($.roi.arrTimeSaved.value + " Hours" );
-		$($.roi.arrCostSaved.slider).val("$" + $.roi.arrCostSaved.value);
+		iLaggardWallet = $.industVar.wallet.low[$.dropdownVars.industry.value];
+		iLeaderWallet = $.industVar.wallet.high[$.dropdownVars.industry.value];
+		iDiffWallet = iLeaderWallet - iLaggardWallet
+		$($.fieldVars.wallet.laggards).val(iLaggardWallet + "%");
+		$($.fieldVars.wallet.leaders).val(iLeaderWallet + "%");
+		$($.fieldVars.wallet.diff).val(iDiffWallet + "%");
 	};
 	
+	// Prints the output to the screen on the page load
+	calcOutput();
 	
 });
